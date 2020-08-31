5f95fb4965e433f08b7337d4961a60bfbe513667
Tim Eitel <timeitel@outlook.com>
2020-09-01 00:41:18 +0800

    style: landing and masthead

--- components/landing.tsx
+++ components/landing.tsx
@@ -1,7 +1,16 @@
+import styles from '../styles/Landing.module.css';
+
 export default function Landing() {
   return (
-    <section className='h-screen w-full'>
-      <h1 className='text-c-white grid'>Landing</h1>
+    <section className={styles.masthead}>
+      <div className={styles.masthead__group}>
+        <h2 className='whitespace-no-wrap'>Hi there,</h2>
+        <h2>I'm</h2>
+      </div>
+      <div className={styles.title}>
+        <h1>Tim Eitel</h1>
+        <h4>Software Engineer</h4>
+      </div>
     </section>
   );
 }
