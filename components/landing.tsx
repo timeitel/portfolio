b259e9e68905254abb13be1f303a4dd0218f5534
Tim Eitel <timeitel@outlook.com>
2020-09-03 00:00:38 +0800

    style: landing title positioning

--- components/landing.tsx
+++ components/landing.tsx
@@ -2,9 +2,9 @@ import styles from '../styles/Landing.module.css';
 
 export default function Landing() {
   return (
-    <section className={styles.masthead}>
-      <div className={styles.masthead__group}>
-        <h2 className='whitespace-no-wrap'>Hi there,</h2>
+    <section className='h-screen w-full relative'>
+      <div className={styles.intro}>
+        <h2 className={styles.intro__hi}>Hi there,</h2>
         <h2>I'm</h2>
       </div>
       <div className={styles.title}>
