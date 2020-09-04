e51b7c347220bcc424b8eaa472ff99b1b8eee1df
Tim Eitel <timeitel@outlook.com>
2020-09-04 19:47:45 +0800

    style: navbar media query and menu icon

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,9 +5,7 @@ export default function Navbar() {
 
   return (
     <header className={styles.header}>
-      <div>
-        <span>{logo}</span>
-      </div>
+      <a href='/'>{logo}</a>
       <nav className='flex'>
         <ul className={styles.nav__ul}>
           <li className={styles.nav__li}>
@@ -30,6 +28,19 @@ export default function Navbar() {
             >
               Resume
             </a>
+          </li>
+          <li>
+            <svg
+              viewBox='0 0 20 20'
+              fill='currentColor'
+              className='menu-alt4 w-6 h-6'
+            >
+              <path
+                fill-rule='evenodd'
+                d='M3 7a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 13a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z'
+                clip-rule='evenodd'
+              ></path>
+            </svg>
           </li>
         </ul>
       </nav>
