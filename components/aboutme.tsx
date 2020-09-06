d480cf3cdd2424143c25138ed6b2254b584bb3aa
Tim Eitel <timeitel@outlook.com>
2020-09-06 14:38:34 +0800

    add: about section content

--- components/aboutme.tsx
+++ components/aboutme.tsx
@@ -1,7 +1,34 @@
+import styles from '../assets/styles/About.module.css';
+
 export default function AboutMe() {
   return (
-    <section id='about' className='h-screen bg-blue-300'>
-      <h1 className='font-black'>About Me</h1>
+    <section id={styles.about} className='h-screen bg-blue-300 section__header'>
+      <div>
+        <h1 className='font-black section__title'>About Me</h1>
+        <p>Hi there, I'm Tim. I'm a software engineer based in Perth, WA.</p>
+        <p>
+          I enjoy building fast and beautiful apps for the internet, for all
+          devices from desktop to mobile.
+        </p>
+        <p>
+          During and after graduating from the University of Western Australia,
+          I've been working with a team in the RPS Energy segment. From here, I
+          get to work on both .NET enterprise and modern JavaScript single-page
+          application projects.
+        </p>
+        <p>
+          Lately, I've been working on data modelling, data visualization and
+          data-rich apps with these technologies:
+        </p>
+      </div>
+      <ul className={styles.list}>
+        <li>JavaScript (ES6+)</li>
+        <li>HTML & CSS</li>
+        <li>React</li>
+        <li>Vue</li>
+        <li>.NET</li>
+        <li>Dart / Flutter</li>
+      </ul>
     </section>
   );
 }
