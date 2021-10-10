0f207ff11d5efe0b06e814cff47b122f4c505f5d
Tim Eitel <timeitel@outlook.com>
2021-10-10 15:24:14 +0800

    feat: navbar mobile menu

--- components/Home/Navbar/navbar.tsx
+++ components/Home/Navbar/navbar.tsx
@@ -1,55 +1,46 @@
-import React, { FC } from 'react';
-import * as S from '../../shared/styles';
-import styles from './Navbar.module.scss';
+import React, { FC } from "react";
+import * as S from "../../shared/styles";
+import styles from "./Navbar.module.scss";
 
 const Navbar: FC<{}> = () => {
-  const logo = 'Tim { Eitel }';
+  const logo = "Tim { Eitel }";
 
   return (
     <header className={styles.header}>
-      <a href='/' className={styles.logo}>
+      <a href="/" className={styles.logo}>
         {logo}
       </a>
-      <nav className='flex'>
+      <nav className="flex">
         <ul className={styles.nav__ul}>
           <li className={styles.nav__li}>
-            <a href='#about'>About</a>
+            <a href="#about">About</a>
           </li>
           <li className={styles.nav__li}>
-            <a href='#experience'>Experience</a>
+            <a href="#experience">Experience</a>
           </li>
           <li className={styles.nav__li}>
-            <a href='#portfolio'>Portfolio</a>
+            <a href="#portfolio">Portfolio</a>
           </li>
           <li className={styles.nav__li}>
-            <a href='#contact'>Contact</a>
+            <a href="#contact">Contact</a>
           </li>
           <li>
             <S.Link
-              className='flex'
-              target='_blank'
-              href='Tim-Eitel-Resume.pdf'
+              className="flex"
+              target="_blank"
+              href="Tim-Eitel-Resume.pdf"
             >
               Resume
               <svg
-                className='w-6 h-6 ml-2'
-                fill='none'
-                stroke='currentColor'
-                viewBox='0 0 24 24'
-                xmlns='http://www.w3.org/2000/svg'
+                className="w-6 h-6 ml-2"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                <path d='M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14'></path>
+                <path d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"></path>
               </svg>
             </S.Link>
-          </li>
-          <li>
-            <svg
-              viewBox='0 0 20 20'
-              fill='currentColor'
-              className={`w-8 h-8 ml-4 ${styles.menu}`}
-            >
-              <path d='M3 7a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 13a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z'></path>
-            </svg>
           </li>
         </ul>
       </nav>
