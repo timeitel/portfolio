f2f3550a774918b727885402ceb01f94474255d8
timeitel <timeitel@outlook.com>
2022-01-09 15:31:47 +0000

    Deploying to gh-pages from @ timeitel/portfolio@d9ea19a8ef0af972233d8232b119f1dba947df94 🚀

---
+++ _next/static/-NoJ0ddgbwLCCnvucCl4h/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-dd89ba28daf36e9e7bfe.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-9f98e75d88ca90cfa3c4.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
