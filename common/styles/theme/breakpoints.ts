c6e20ffa025b00350b13560bab374c5ab5594f24
Tim Eitel <timeitel@outlook.com>
2022-01-22 18:02:46 +0800

    feat: breakpoints!

--- common/styles/theme/breakpoints.ts
+++ common/styles/theme/breakpoints.ts
@@ -16,31 +16,31 @@ export const breakpointOperators: IBreakpointOperators = {
 
 type BreakpointSize = keyof IBreakpoints;
 export interface IBreakpoints {
-  xs: string;
-  sm: string;
-  md: string;
-  lg: string;
-  xl: string;
+  phone: string;
+  tablet: string;
+  laptop: string;
+  desktop: string;
+  ultraWide: string;
 }
-export const breakpointSizes = {
-  xs: "0px", // phone
-  sm: "600px", // tablet
-  md: "900px", // small laptop
-  lg: "1200px", // desktop
-  xl: "1536px", // large screen
+export const breakpointSizes: IBreakpoints = {
+  phone: "0px",
+  tablet: "600px",
+  laptop: "900px",
+  desktop: "1200px",
+  ultraWide: "1536px",
 };
 
 const getDeviceMediaQuery = (size: BreakpointSize) => {
   switch (size) {
-    case "xs":
-      return getMediaQuery({ min: size, max: "sm" });
-    case "sm":
-      return getMediaQuery({ min: size, max: "md" });
-    case "md":
-      return getMediaQuery({ min: size, max: "lg" });
-    case "lg":
-      return getMediaQuery({ min: size, max: "xl" });
-    case "xl":
+    case "phone":
+      return getMediaQuery({ min: size, max: "tablet" });
+    case "tablet":
+      return getMediaQuery({ min: size, max: "laptop" });
+    case "laptop":
+      return getMediaQuery({ min: size, max: "desktop" });
+    case "desktop":
+      return getMediaQuery({ min: size, max: "ultraWide" });
+    case "ultraWide":
       return getMediaQuery({ min: size });
     default:
       throw Error("Device size not supported.");
@@ -53,11 +53,11 @@ type IGetMediaQuery = AtLeastOne<{
 }>;
 const getMediaQuery = ({ min, max }: IGetMediaQuery) => {
   if (min && max) {
-    return `@media (min-width:${breakpointSizes[min]}px) and (max-width:${breakpointSizes[max]}px)`;
+    return `@media (min-width:${breakpointSizes[min]}) and (max-width:${breakpointSizes[max]})`;
   } else if (min) {
-    return `@media (min-width:${breakpointSizes[min]}px)`;
+    return `@media (min-width:${breakpointSizes[min]})`;
   } else if (max) {
-    return `@media (max-width:${breakpointSizes[max]}px)`;
+    return `@media (max-width:${breakpointSizes[max]})`;
   } else {
     throw Error("At least min or max required.");
   }
