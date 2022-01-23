913285c018b8fa91c1d4631e6ace2d0570d223a8
Tim Eitel <timeitel@outlook.com>
2022-01-23 16:20:34 +0800

    style: animate job description

--- modules/Jobs/Job.tsx
+++ modules/Jobs/Job.tsx
@@ -1,5 +1,6 @@
 import { useTheme } from "@emotion/react";
 import React, { FC } from "react";
+import { animated, useTransition } from "react-spring";
 import { StyledDutiesList, StyledJobTime, StyledRole } from "./styled";
 
 interface Props {
@@ -11,6 +12,11 @@ interface Props {
 }
 
 export const Job: FC<Props> = ({ active, duties, title, company, time }) => {
+  const transitions = useTransition(active, {
+    from: { opacity: 0, transform: `translate3d(25px, 25px, 0)` },
+    enter: { opacity: 1, transform: `translate3d(0px, 0px, 0)` },
+    leave: { opacity: 0, transform: `translate3d(25px, 25px, 0)` },
+  });
   const {
     color: { blue600 },
   } = useTheme();
@@ -22,16 +28,19 @@ export const Job: FC<Props> = ({ active, duties, title, company, time }) => {
     </li>
   ));
 
-  return (
-    <div className="job-description-container">
-      <StyledRole>
-        <span>{title}</span>
-        <span style={{ color: blue600 }}>
-          <span style={{ whiteSpace: "nowrap" }}> @ {company}</span>
-        </span>
-      </StyledRole>
-      <StyledJobTime>{time}</StyledJobTime>
-      <StyledDutiesList>{dutyItems}</StyledDutiesList>
-    </div>
+  return transitions(
+    (styles, show) =>
+      show && (
+        <animated.div className="job-description-container" style={styles}>
+          <StyledRole>
+            <span>{title}</span>
+            <span style={{ color: blue600 }}>
+              <span style={{ whiteSpace: "nowrap" }}> @ {company}</span>
+            </span>
+          </StyledRole>
+          <StyledJobTime>{time}</StyledJobTime>
+          <StyledDutiesList>{dutyItems}</StyledDutiesList>
+        </animated.div>
+      )
   );
 };
