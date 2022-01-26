4fac0fed1d3cac981dade94cc3f3c38438d5d0ee
Tim Eitel <timeitel@outlook.com>
2022-01-26 10:58:36 +0800

    feat: button variants

--- common/components/Button/Button.tsx
+++ common/components/Button/Button.tsx
@@ -1,6 +1,7 @@
 import { ButtonProps, ButtonSizes } from "common/components/Button";
 import styled from "@emotion/styled";
 import { FC } from "react";
+import { ButtonVariants } from "@components/Button/types";
 
 export const Button: FC<ButtonProps> = ({ children, ...rest }) => {
   return <StyledDefaultButton {...rest}>{children}</StyledDefaultButton>;
@@ -9,19 +10,26 @@ export const Button: FC<ButtonProps> = ({ children, ...rest }) => {
 const StyledDefaultButton = styled.button<ButtonProps>`
   align-items: center;
   background-color: inherit;
-  color: inherit;
   border-radius: 4px;
   border: none;
+  color: inherit;
   cursor: pointer;
   display: inline-flex;
-  line-height: 1.75;
-  justify-content: center;
   font-weight: 500;
-  padding: ${(p) =>
-    p.size ? ButtonSizes[p.size].padding : ButtonSizes.md.padding};
-  font-size: ${(p) =>
-    p.size ? ButtonSizes[p.size].fontSize : ButtonSizes.md.fontSize};
+  justify-content: center;
   letter-spacing: 0.02857em;
+  line-height: 1.75;
   position: relative;
   transition: background 0.25s ease-in 0.15s;
+
+  &:hover {
+    background-color: ${(p) => p.theme.color.grey400};
+  }
+
+  ${(p) => p.size && { ...ButtonSizes[p.size] }};
+  ${(p) => p.variant && { ...ButtonVariants[p.variant] }};
 `;
+StyledDefaultButton.defaultProps = {
+  size: "md",
+  variant: "text",
+};
--- common/components/Button/types.ts
+++ common/components/Button/types.ts
@@ -1,7 +1,9 @@
+import { themeDark } from "@styles/theme";
 import { CSSProperties, HTMLAttributes } from "react";
 
 export interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
-  size?: keyof typeof ButtonSizes;
+  size?: keyof IButtonSizes;
+  variant?: keyof IButtonVariants;
 }
 
 interface IButtonSizes {
@@ -17,4 +19,16 @@ export const ButtonSizes: IButtonSizes = {
   },
   md: { padding: "6px 16px", fontSize: "1rem" },
   lg: { padding: "8px 22px", fontSize: "1.0625rem" },
+};
+
+type IButtonVariants = {
+  text: CSSProperties;
+  contained: CSSProperties;
+  outlined: CSSProperties;
+};
+
+export const ButtonVariants: IButtonVariants = {
+  text: {},
+  contained: { backgroundColor: themeDark.color.blue200 },
+  outlined: { border: "1px solid" },
 };
