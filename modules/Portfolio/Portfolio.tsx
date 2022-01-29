01ea7bcc3927b6372a065c0a44784358619e694e
Tim Eitel <timeitel@outlook.com>
2022-01-29 10:51:41 +0800

    style: portfolio container

--- modules/Portfolio/Portfolio.tsx
+++ modules/Portfolio/Portfolio.tsx
@@ -36,41 +36,43 @@ export const Portfolio: FC<Props> = ({ projects }) => {
 
   return (
     <StyledSection id="portfolio" backgroundColor="blackPrimary">
-      <animated.div style={fadeInStyle} ref={intersectionRef}>
-        <hgroup className="portfolio-open-tag" style={{ margin: "0 auto" }}>
-          <h3 className="section__subtitle" style={{ color: whitePrimary }}>
-            Some Things I've Built
-          </h3>
-        </hgroup>
-      </animated.div>
-      {projectList}
-      <animated.div style={seeMoreFadeInStyle} ref={seeMoreIntersectionRef}>
-        <div
-          style={{
-            textAlign: "center",
-            color: whitePrimary,
-            marginBottom: "4rem",
-          }}
-          className="portfolio-closing-tag"
-        >
-          <h6 style={{ marginTop: "5rem" }} className="section__subtitle">
-            Want to see more of my projects?
-          </h6>
-          <Link
-            href="https://github.com/timeitel/"
-            target="_blank"
+      <div style={{ margin: "auto", maxWidth: "1250px" }}>
+        <animated.div style={fadeInStyle} ref={intersectionRef}>
+          <hgroup className="portfolio-open-tag" style={{ margin: "0 auto" }}>
+            <h3 className="section__subtitle" style={{ color: whitePrimary }}>
+              Some Things I've Built
+            </h3>
+          </hgroup>
+        </animated.div>
+        {projectList}
+        <animated.div style={seeMoreFadeInStyle} ref={seeMoreIntersectionRef}>
+          <div
             style={{
-              margin: "1rem auto",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
+              textAlign: "center",
+              color: whitePrimary,
+              marginBottom: "4rem",
             }}
+            className="portfolio-closing-tag"
           >
-            <ExternalLinkIcon />
-            <span style={{ marginLeft: "0.25rem" }}>Browse</span>
-          </Link>
-        </div>
-      </animated.div>
+            <h6 style={{ marginTop: "5rem" }} className="section__subtitle">
+              Want to see more of my projects?
+            </h6>
+            <Link
+              href="https://github.com/timeitel/"
+              target="_blank"
+              style={{
+                margin: "1rem auto",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <ExternalLinkIcon />
+              <span style={{ marginLeft: "0.25rem" }}>Browse</span>
+            </Link>
+          </div>
+        </animated.div>
+      </div>
     </StyledSection>
   );
 };
