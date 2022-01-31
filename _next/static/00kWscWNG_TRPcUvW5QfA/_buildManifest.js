9d37e00111510a738ce1869d7aa7fa97c4e3fcc4
timeitel <timeitel@outlook.com>
2022-01-31 16:32:46 +0000

    Deploying to gh-pages from @ timeitel/portfolio@a606455e5c10d52db2965345b2f15f855283f81b 🚀

---
+++ _next/static/00kWscWNG_TRPcUvW5QfA/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/95-b1c68019ab51b45a.js","static/chunks/pages/index-3d453b0e406f98b6.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
